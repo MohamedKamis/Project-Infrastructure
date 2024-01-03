eb2e0cd65067b5570c2e425bb292f9ed134bd50e
MohamedKhamis <112751426+MohamedKamis@users.noreply.github.com>
2024-01-03 08:16:58 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,21 @@ const port = 8080;
 app.use(express.json());
 
 app.get('/', async (_req, res) => {
-  res.send({v:'secsusfoly'});
-});
+  res.send("
+const accountSid = 'YOUR_TWILIO_ACCOUNT_SID';
+const authToken = 'YOUR_TWILIO_AUTH_TOKEN';
+const client = require('twilio')(accountSid, authToken);
 
+client.messages
+  .create({
+    body: 'Hello from Node.js!',
+    from: 'whatsapp:+14155238886', // This is your Twilio sandbox number
+    to: 'whatsapp:+1234567890' // This is the recipient's number
+  })
+  .then(message => console.log(message.sid))
+  .catch(error => console.log(error));
+");
+});
 app.listen(port, () => {
   console.log(`Your server starting on --> http://localhost:${port}?id=`);
 });
